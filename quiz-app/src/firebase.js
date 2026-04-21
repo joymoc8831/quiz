561661ac3bad9cfce00e1143f3a977b02fac11b7
MORO CHINATSU <joymoc8831@gmail.com>
2026-04-21 23:19:55 +0900

    Update firebase.js

--- quiz-app/src/firebase.js
+++ quiz-app/src/firebase.js
@@ -5,12 +5,27 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDslW42ydu8c5IwUwSaAvNac-DtudfmvGE",
+  authDomain: "quiz-f8ee3.firebaseapp.com",
+  projectId: "quiz-f8ee3",
+  storageBucket: "quiz-f8ee3.firebasestorage.app",
+  messagingSenderId: "263683850540",
+  appId: "1:263683850540:web:ef919dfae0b1763c95ec89",
+  measurementId: "G-8ZZQXZN3D5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 };
 
 const app = initializeApp(firebaseConfig);
